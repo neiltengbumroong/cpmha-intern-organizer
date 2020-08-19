b7ee9f4098ee357fd25e3cbe8cf1e6502aa5b373
Neil Tengbumroong <tengneil99@gmail.com>
2020-08-18 20:34:01 -0700

    temporary fix for master key

--- client/src/utils/key.js
+++ client/src/utils/key.js
@@ -1,2 +1,4 @@
-const MASTER_KEY = window.location.hostname === 'localhost' ? 'paolo' : process.env.REACT_APP_MASTER_KEY;
+// const MASTER_KEY = window.location.hostname === 'localhost' ? 'paolo' : process.env.REACT_APP_MASTER_KEY;
+// const MASTER_KEY = process.env.REACT_APP_MASTER_KEY;
+const MASTER_KEY = 'paolo';
 export default MASTER_KEY;
