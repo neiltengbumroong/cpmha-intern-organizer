059207c14a08d1e43011be1ac6ee715a3628afe7
Neil Tengbumroong <tengneil99@gmail.com>
2020-08-18 20:15:13 -0700

    maybe show master key

--- client/src/utils/key.js
+++ client/src/utils/key.js
@@ -1,2 +1,2 @@
-const MASTER_KEY = window.location.hostname === 'localhost' ? 'paolo' : process.env.MASTER_KEY;
+const MASTER_KEY = window.location.hostname === 'localhost' ? 'paolo' : process.env.REACT_APP_MASTER_KEY;
 export default MASTER_KEY;
