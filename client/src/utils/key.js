892f4273ad75ebaabf905d0b8344cf1120930c35
Neil Tengbumroong <tengneil99@gmail.com>
2020-08-18 20:53:04 -0700

    almost prepped for testing

--- client/src/utils/key.js
+++ client/src/utils/key.js
@@ -1,4 +1,2 @@
-// const MASTER_KEY = window.location.hostname === 'localhost' ? 'paolo' : process.env.REACT_APP_MASTER_KEY;
-// const MASTER_KEY = process.env.REACT_APP_MASTER_KEY;
-const MASTER_KEY = 'paolo';
+const MASTER_KEY = window.location.hostname === 'localhost' ? 'paolo' : process.env.REACT_APP_MASTER_KEY;
 export default MASTER_KEY;
